d3b164b1a6f0dd602fc7e8e5bb9a536d14a33079
alamin-karno <amin15-1951@diu.edu.bd>
2022-11-27 14:08:10 +0600

    E-Mart: Cart Screen Text Added

--- lib/views/cart_screen/cart_screen.dart
+++ lib/views/cart_screen/cart_screen.dart
@@ -6,7 +6,12 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.greenAccent,
+      color: Colors.white,
+      child: 'Cart is empty'
+          .text
+          .fontFamily(semibold)
+          .color(darkFontGrey)
+          .makeCentered(),
     );
   }
 }
--- lib/views/category_screen/category_screen.dart
+++ lib/views/category_screen/category_screen.dart
@@ -18,47 +18,49 @@ class CategoryScreen extends StatelessWidget {
         ),
         body: Container(
           padding: const EdgeInsets.symmetric(horizontal: 12),
-          child: GridView.builder(
-            shrinkWrap: true,
-            itemCount: categoryTitleList.length,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              mainAxisSpacing: 8,
-              crossAxisSpacing: 8,
-              mainAxisExtent: 180,
-            ),
-            itemBuilder: (context, index) {
-              return Column(
-                children: [
-                  Image.asset(
-                    categoryImageList[index],
-                    height: 120,
-                    width: 200,
-                    fit: BoxFit.cover,
-                  ),
-                  5.heightBox,
-                  categoryTitleList[index]
-                      .text
-                      .color(darkFontGrey)
-                      .align(TextAlign.center)
-                      .make(),
-                ],
-              )
-                  .box
-                  .roundedSM
-                  .clip(Clip.antiAlias)
-                  .padding(const EdgeInsets.all(8.0))
-                  .white
-                  .outerShadowSm
-                  .make()
-                  .onTap(
-                    () => Get.to(
-                      () => CategoryDetailsScreen(
-                        title: categoryTitleList[index],
-                      ),
+          child: Center(
+            child: GridView.builder(
+              shrinkWrap: true,
+              itemCount: categoryTitleList.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing: 8,
+                crossAxisSpacing: 8,
+                mainAxisExtent: 180,
+              ),
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    Image.asset(
+                      categoryImageList[index],
+                      height: 120,
+                      width: 200,
+                      fit: BoxFit.cover,
                     ),
-                  );
-            },
+                    5.heightBox,
+                    categoryTitleList[index]
+                        .text
+                        .color(darkFontGrey)
+                        .align(TextAlign.center)
+                        .make(),
+                  ],
+                )
+                    .box
+                    .roundedSM
+                    .clip(Clip.antiAlias)
+                    .padding(const EdgeInsets.all(8.0))
+                    .white
+                    .outerShadowSm
+                    .make()
+                    .onTap(
+                      () => Get.to(
+                        () => CategoryDetailsScreen(
+                          title: categoryTitleList[index],
+                        ),
+                      ),
+                    );
+              },
+            ),
           ),
         ),
       ),
