665b81034c18b459813637522c9ee1f7eeba28d9
alamin-karno <amin15-1951@diu.edu.bd>
2022-12-01 16:40:36 +0600

    E-Mart: PROFILE PIC ERROR FIX

--- lib/consts/strings.dart
+++ lib/consts/strings.dart
@@ -79,3 +79,6 @@ const wishList = 'Wishlist';
 const myMessages = 'My Messages';
 const myOrders = 'My Orders';
 const myWishList = 'My WishList';
+
+const profileImage =
+    'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png';
--- lib/views/account_screen/account_screen.dart
+++ lib/views/account_screen/account_screen.dart
@@ -55,7 +55,7 @@ class AccountScreen extends StatelessWidget {
                       children: [
                         Image.network(
                           data['imageUrl'] == ''
-                              ? 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png'
+                              ? profileImage
                               : '${data['imageUrl']}',
                           fit: BoxFit.cover,
                           width: 70,
